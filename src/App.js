f89a1e11059f9e2551f1919ed875922e84e9ea5b
Amir <apahadi73@gmail.com>
2021-02-01 04:00:29 -0600

    tired lol

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AllBlogs from "./Pages/Blogs";
-import HomePage from "./Pages/HomePage";
 import Footer from "./components/Navigations/Footer";
 import Header from "./components/Navigations/Header";
 import ChatRoom from "./Pages/ChatRoomPage";
 import AboutCompanyPage from "./Pages/AboutCompanyPage";
 import PostDetailPage from "./Pages/PostDetailPage";
+import Homepage from "./Pages/HomePage";
 
 export default function App() {
   return (
     <Router>
       <Header />
       <main className="py-3">
-        <Route path="/" component={HomePage} exact />
+        <Route path="/" component={Homepage} exact />
         <Route path="/posts" component={AllBlogs} exact />
         <Route path="/posts/:id" component={PostDetailPage} exact />
         <Route path="/chat-room" component={ChatRoom} exact />
